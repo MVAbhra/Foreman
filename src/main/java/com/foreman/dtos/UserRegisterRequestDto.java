fd15fc70386f2830153bd0454f3a493bcbf2ed02
Abhra Chowdhury <52403613+MVAbhra@users.noreply.github.com>
2026-08-06 13:19:52 +0530

    Add Pattern validation import to UserRegisterRequestDto

--- src/main/java/com/foreman/dtos/UserRegisterRequestDto.java
+++ src/main/java/com/foreman/dtos/UserRegisterRequestDto.java
@@ -3,6 +3,7 @@ package com.foreman.dtos;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
